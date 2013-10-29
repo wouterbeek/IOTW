1c3f4b4a1f28341a61146b60dd2cba3ea583daea
Wouter Beek <me@wouterbeek.com>
2013-10-29 16:29:30 +0100

    * [DOC] Added README file.

--- iotw.pl
+++ iotw.pl
@@ -13,24 +13,6 @@
 
 IOTW experiments.
 
-## Changes in the 2013/08-2013/09 version w.r.t. the 2013/05 version
-
-  * Optimization using association lists implemented by AVL trees
-    containing ordered sets.
-  * Identity sets i.o. identity pairs.
-  * Simple, RDF, and RDFS entailment.
-  * Predicate/Object-pair subpartitions are calculated.
-  * Literal identity is established in terms of the value space
-    (used to be based on the lexical space).
-  * Modular rewrite of all IOTW code.
-  * GraphViz export was ported to the new DCG-based implementation.
-
-## TODO
-
-  * JS callback  for predicate/object-pairs and rough set quality.
-  * Multi-agent materialization.
-  * SKOS and OWL identity reasoning.
-
 Recommendation sharing non-monotonic?
 
 @author Wouter Beek
