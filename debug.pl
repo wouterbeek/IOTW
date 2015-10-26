1ae7bba2e6b8e5c0c616f89958751cf0e2b16cf0
Wouter Beek <me@wouterbeek.com>
2015-10-26 08:57:56 +0100

    DEPLOYMENT: Set file search path `iotw'.

--- debug.pl
+++ debug.pl
@@ -6,9 +6,4 @@
 :- debug(iotw).
 :- debug(oaei).
 
-:- use_module(library(apply)).
-
-:- use_module(iotw_iimb).
-
-:- iimb_experiment(N, Svg),
-   maplist(writeln, [N,Svg]).
+:- [load].
--- load.pl
+++ load.pl
@@ -1,12 +1,21 @@
-:- set_prolog_stack(global, limit(2*10**9)).
-
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_server)).
-
-:- start_server.
+:- use_module(library(profile/profile_rdf)).
 
 :- dynamic(user:file_search_path/2).
 :- multifile(user:file_search_path/2).
 
 user:file_search_path(css, resource/css).
 user:file_search_path(js, resource/js).
+
+init_iotw:-
+  source_file(init_iotw, File),
+  file_directory_name(File, Dir),
+  assert(user:file_search_path(iotw, Dir)),
+  start_server.
+:- init_iotw.
+
+:- use_module(iotw(iotw_iimb)).
+
+:- iimb_experiment(N, Svg),
+   maplist(writeln, [N,Svg]).
